fb31eb30869bdbd1397bad3581e1c97adedf0e9e
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2019-11-08 22:09:06 +0000

    Make sure examples use all the volumes in the test image.

--- examples/create_test.rs
+++ examples/create_test.rs
@@ -112,91 +112,93 @@ impl TimeSource for Clock {
 fn main() {
     env_logger::init();
     let mut args = std::env::args().skip(1);
-    let filename = args.next().unwrap_or("/dev/mmcblk0".into());
+    let filename = args.next().unwrap_or_else(|| "/dev/mmcblk0".into());
     let print_blocks = args.find(|x| x == "-v").map(|_| true).unwrap_or(false);
     let lbd = LinuxBlockDevice::new(filename, print_blocks)
         .map_err(Error::DeviceError)
         .unwrap();
     println!("lbd: {:?}", lbd);
     let mut controller = Controller::new(lbd, Clock);
-    let volume = controller.get_volume(VolumeIdx(0));
-    println!("volume {}: {:#?}", 0, volume);
-    if let Ok(mut volume) = volume {
-        let root_dir = controller.open_root_dir(&volume).unwrap();
-        println!("\tListing root directory:");
-        controller
-            .iterate_dir(&volume, &root_dir, |x| {
-                println!("\t\tFound: {:?}", x);
-            })
-            .unwrap();
-        println!("\nCreating file {}...", FILE_TO_CREATE);
-        // This will panic if the file already exists, use ReadWriteCreateOrAppend or
-        // ReadWriteCreateOrTruncate instead
-        let mut f = controller
-            .open_file_in_dir(
-                &mut volume,
-                &root_dir,
-                FILE_TO_CREATE,
-                Mode::ReadWriteCreate,
-            )
-            .unwrap();
-        println!("\nReading from file");
-        println!("FILE STARTS:");
-        while !f.eof() {
-            let mut buffer = [0u8; 32];
-            let num_read = controller.read(&volume, &mut f, &mut buffer).unwrap();
-            for b in &buffer[0..num_read] {
-                if *b == 10 {
-                    print!("\\n");
+    for volume_idx in 0..=3 {
+        let volume = controller.get_volume(VolumeIdx(volume_idx));
+        println!("volume {}: {:#?}", volume_idx, volume);
+        if let Ok(mut volume) = volume {
+            let root_dir = controller.open_root_dir(&volume).unwrap();
+            println!("\tListing root directory:");
+            controller
+                .iterate_dir(&volume, &root_dir, |x| {
+                    println!("\t\tFound: {:?}", x);
+                })
+                .unwrap();
+            println!("\nCreating file {}...", FILE_TO_CREATE);
+            // This will panic if the file already exists, use ReadWriteCreateOrAppend or
+            // ReadWriteCreateOrTruncate instead
+            let mut f = controller
+                .open_file_in_dir(
+                    &mut volume,
+                    &root_dir,
+                    FILE_TO_CREATE,
+                    Mode::ReadWriteCreate,
+                )
+                .unwrap();
+            println!("\nReading from file");
+            println!("FILE STARTS:");
+            while !f.eof() {
+                let mut buffer = [0u8; 32];
+                let num_read = controller.read(&volume, &mut f, &mut buffer).unwrap();
+                for b in &buffer[0..num_read] {
+                    if *b == 10 {
+                        print!("\\n");
+                    }
+                    print!("{}", *b as char);
                 }
-                print!("{}", *b as char);
             }
-        }
-        println!("EOF");
+            println!("EOF");
 
-        let buffer1 = b"\nFile Appended\n";
-        let mut buffer: Vec<u8> = vec![];
-        for _ in 0..64 {
-            for _ in 0..15 {
-                buffer.push(b'a');
-            }
-            buffer.push(b'\n');
-        }
-        println!("\nAppending to file");
-        let num_written1 = controller.write(&mut volume, &mut f, &buffer1[..]).unwrap();
-        let num_written = controller.write(&mut volume, &mut f, &buffer[..]).unwrap();
-        println!("Number of bytes written: {}\n", num_written + num_written1);
-        controller.close_file(&volume, f).unwrap();
-
-        let mut f = controller
-            .open_file_in_dir(
-                &mut volume,
-                &root_dir,
-                FILE_TO_CREATE,
-                Mode::ReadWriteCreateOrAppend,
-            )
-            .unwrap();
-        f.seek_from_start(0).unwrap();
-
-        println!("\tFinding {}...", FILE_TO_CREATE);
-        println!(
-            "\tFound {}?: {:?}",
-            FILE_TO_CREATE,
-            controller.find_directory_entry(&volume, &root_dir, FILE_TO_CREATE)
-        );
-        println!("\nReading from file");
-        println!("FILE STARTS:");
-        while !f.eof() {
-            let mut buffer = [0u8; 32];
-            let num_read = controller.read(&volume, &mut f, &mut buffer).unwrap();
-            for b in &buffer[0..num_read] {
-                if *b == 10 {
-                    print!("\\n");
+            let buffer1 = b"\nFile Appended\n";
+            let mut buffer: Vec<u8> = vec![];
+            for _ in 0..64 {
+                for _ in 0..15 {
+                    buffer.push(b'a');
                 }
-                print!("{}", *b as char);
+                buffer.push(b'\n');
             }
+            println!("\nAppending to file");
+            let num_written1 = controller.write(&mut volume, &mut f, &buffer1[..]).unwrap();
+            let num_written = controller.write(&mut volume, &mut f, &buffer[..]).unwrap();
+            println!("Number of bytes written: {}\n", num_written + num_written1);
+            controller.close_file(&volume, f).unwrap();
+
+            let mut f = controller
+                .open_file_in_dir(
+                    &mut volume,
+                    &root_dir,
+                    FILE_TO_CREATE,
+                    Mode::ReadWriteCreateOrAppend,
+                )
+                .unwrap();
+            f.seek_from_start(0).unwrap();
+
+            println!("\tFinding {}...", FILE_TO_CREATE);
+            println!(
+                "\tFound {}?: {:?}",
+                FILE_TO_CREATE,
+                controller.find_directory_entry(&volume, &root_dir, FILE_TO_CREATE)
+            );
+            println!("\nReading from file");
+            println!("FILE STARTS:");
+            while !f.eof() {
+                let mut buffer = [0u8; 32];
+                let num_read = controller.read(&volume, &mut f, &mut buffer).unwrap();
+                for b in &buffer[0..num_read] {
+                    if *b == 10 {
+                        print!("\\n");
+                    }
+                    print!("{}", *b as char);
+                }
+            }
+            println!("EOF");
+            controller.close_file(&volume, f).unwrap();
         }
-        println!("EOF");
-        controller.close_file(&volume, f).unwrap();
     }
 }
