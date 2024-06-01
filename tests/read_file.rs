0de58d3e8a84dd37b3e03d3a596490226ecda6d2
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2024-06-01 19:52:14 +0100

    Move new test into read_file.

--- tests/read_file.rs
+++ tests/read_file.rs
@@ -184,6 +184,27 @@ fn read_file_backwards() {
     assert_eq!(&hash[..], TEST_DAT_SHA256_SUM);
 }
 
+#[test]
+fn read_file_with_odd_seek() {
+    let time_source = utils::make_time_source();
+    let disk = utils::make_block_device(utils::DISK_SOURCE).unwrap();
+    let mut volume_mgr = embedded_sdmmc::VolumeManager::new(disk, time_source);
+
+    let mut volume = volume_mgr
+        .open_volume(embedded_sdmmc::VolumeIdx(0))
+        .unwrap();
+    let mut root_dir = volume.open_root_dir().unwrap();
+    let mut f = root_dir
+        .open_file_in_dir("64MB.DAT", embedded_sdmmc::Mode::ReadOnly)
+        .unwrap();
+    f.seek_from_start(0x2c).unwrap();
+    while f.offset() < 1000000 {
+        let mut buffer = [0u8; 2048];
+        f.read(&mut buffer).unwrap();
+        f.seek_from_current(-1024).unwrap();
+    }
+}
+
 // ****************************************************************************
 //
 // End Of File
